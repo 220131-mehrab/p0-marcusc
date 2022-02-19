d1aa54c5fab798d60e2feb1ba4a16702acd9c18b
Marcus Creese <marcuscreese94@gmail.com>
2022-02-19 10:58:05 -0600

    adding files to git ignore

--- src/main/java/com/revature/cardex/App.java
+++ src/main/java/com/revature/cardex/App.java
@@ -2,6 +2,8 @@ package com.revature.cardex;
 
 
 import com.revature.cardex.repository.CarRepository;
+import org.apache.catalina.LifecycleException;
+import org.apache.catalina.startup.Tomcat;
 
 //making Java read the csv file
 //work backwards build from ground up
@@ -31,9 +33,32 @@ public class App {
          */
         CarService carService = new CarService(carRepository);
         /**
-        3.its only job is to serve http, generic reusable server
+         3.its only job is to serve http, generic reusable server
          */
-        CarServer carServer = new CarServer(carService);
+
+        //this constructor is in charge of handling http
+        //server should handle http
+        //the last thing we need is the server
+        //need a tomcat class
+        //dont really need the carServer class since we can call on Tomcat
+        Tomcat server = new Tomcat();
+
+            server = new Tomcat();
+            //this will load in Tomcats http stuff
+            server.getConnector();
+            //leaving blank for now
+            server.addContext("", null);
+            //finally add Servlet contextPath and name it and then pass in carService and
+            //and finally .addMapping because it needs a URL.
+            server.addServlet("", "carServlet", carService).addMapping("/");
+            // also add this.server.start and surround in try catch method
+            try {
+                server.start();
+            } catch (LifecycleException e) {
+                e.printStackTrace();
+            }
+            //now I dont need the carServer class anymore so I deleted it.
+        }
     }
-}
+
 

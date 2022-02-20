091fd479e7a407b838f20dbfa3bf1684504d989d
Marcus Creese <marcuscreese94@gmail.com>
2022-02-19 20:13:53 -0600

    commit

--- src/main/java/com/revature/cardex/App.java
+++ src/main/java/com/revature/cardex/App.java
@@ -36,45 +36,22 @@ public class App {
          * 1. a place where our data thats flowing will gather
          * and sync.
          */
-        CarRepository carRepository = new CarRepository("cars.csv");
         /**2.a location where we do business logic,and gather
          *         content the last place to call everything thats needed to run
          */
-        CarService carService = new CarService(carRepository);
         /**
          3.its only job is to serve http, generic reusable server
          */
 
-        //this constructor is in charge of handling http
-        //server should handle http
-        //the last thing we need is the server
-        //need a tomcat class
-        //dont really need the carServer class since we can call on Tomcat
-        Tomcat tomcat = new Tomcat();
-        tomcat.setPort(8080);
-        // Set context path and root folder
-        String contextPath = "/";
-        String docBase = new File(".").getAbsolutePath();
-        Context context = tomcat.addContext(contextPath, docBase);
-        // Declare, define, and map servlets
-        HttpServlet helloServlet = new HttpServlet(){
-            @Override
-            protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-                    throws ServletException, IOException {
-                PrintWriter out = resp.getWriter();
-                out.write("<html><title>Example</title><body><h1>Hello, World!</h1></body></html>");
-                out.close();
-            }
-        };
-        String servletName = "HelloServlet";
-        String urlPattern = "/hello";
-        // Register servlets with Tomcat
-        Tomcat.addServlet(context, servletName, helloServlet);
-        ((Context) context).addServletMappingDecoded(urlPattern, servletName);
-        // Start the server
-        tomcat.start();
-        tomcat.getServer().await();
+        String webAppName = "carDex";
+
+        CarRepository carRepository;
+        carRepository = new CarRepository("cars.csv");
+        CarServer server = new CarServer();
+        try {
+            server.start();
+        } catch (Exception e) {
+
+        }
     }
 }
-
-

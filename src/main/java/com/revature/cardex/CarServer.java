7b9fea1c2fc941fcf8ba96a47d89eaa73d4a1fc8
Marcus Creese <marcuscreese94@gmail.com>
2022-02-19 20:24:54 -0600

    commit

--- src/main/java/com/revature/cardex/CarServer.java
+++ src/main/java/com/revature/cardex/CarServer.java
@@ -1,4 +1,17 @@
 package com.revature.cardex;
 
+import org.eclipse.jetty.server.Connector;
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.ServerConnector;
+
+
+
 public class CarServer {
+    private Server server;
+
+    public void start() throws Exception{
+        ServerConnector connector = new ServerConnector(server);
+            connector.setPort(8090);
+            server.setConnectors(new Connector[] {connector});
+    }
 }
